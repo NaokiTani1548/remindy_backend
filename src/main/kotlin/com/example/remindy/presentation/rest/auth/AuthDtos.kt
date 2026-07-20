75391cca555283f4b9ef2b09bd2ddfda1d1d46a5
NaokiTani <naoki.1121.hit.and.run48@gmail.com>
2026-07-21 00:28:04 +0900

    パスワードのバリデーションを緩く

--- src/main/kotlin/com/example/remindy/presentation/rest/auth/AuthDtos.kt
+++ src/main/kotlin/com/example/remindy/presentation/rest/auth/AuthDtos.kt
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size
 
 data class RegisterRequest(
     @field:NotBlank @field:Email @field:Size(max = 254) val emailAddress: String,
-    @field:NotBlank @field:Size(min = 8, max = 100) val password: String,
+    @field:NotBlank @field:Size(min = 6, max = 100) val password: String,
 )
 
 data class LoginRequest(
